491dfc27f18464da9334f5163aa0a0ee1e957317
Tarrke <tarrke@gmail.com>
2023-04-17 15:52:53 +0200

    adding vite+ts config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+/** @type {import('vite').UserConfig} */
 import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [],
-	server: { host: '0.0.0.0', port: 8000 },
+	server: { 
+		host: '0.0.0.0', 
+		port: 8000 
+	},
 	clearScreen: false,
+	publicDir: './public',
 })
